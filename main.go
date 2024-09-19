d93685e8deae1f8b101b8998a7940c8ab3384b08
4lifbima <alif67916@gmail.com>
2024-09-20 02:44:17 +0800

    Commit Ka dua

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
 import "fmt"
+import "html/template"
+import "net/http"
 
-func main()  {
-	fmt.Println("Hello Word")
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        var data = map[string]string{
+            "Name":    "Alif Bima",
+            "Message": "Hay Hari Ini saya Belajar Bahasa Pemrograman Golang. ini sangat Seru loh,.. Huhuyyy",
+        }
+
+        var t, err = template.ParseFiles("template.html")
+        if err != nil {
+            fmt.Println(err.Error())
+            return
+        }
+
+        t.Execute(w, data)
+    })
+
+    fmt.Println("starting web server at http://localhost:8080/")
+    http.ListenAndServe(":8080", nil)
 }
